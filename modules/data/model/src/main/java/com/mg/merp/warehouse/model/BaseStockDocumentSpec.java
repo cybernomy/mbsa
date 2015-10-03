bd3c61003607b38573fc862c9bc63d85ede9264f
Valentin A. Poroxnenko <valentin.poroxnenko@gmail.com>
2015-10-04 02:55:17 +0300

    MG-5

---
+++ modules/data/model/src/main/java/com/mg/merp/warehouse/model/BaseStockDocumentSpec.java
@@ -0,0 +1,93 @@
+/**
+ * BaseStockDocumentSpec.java
+ *
+ * Copyright (c) 1998 - 2007 BusinessTechnology, Ltd. All rights reserved
+ *
+ * This program is the proprietary and confidential information of BusinessTechnology, Ltd. and may
+ * be used and disclosed only as authorized in a license agreement authorizing and controlling such
+ * use and disclosure
+ *
+ * Millennium Business Suite Anywhere System.
+ */
+package com.mg.merp.warehouse.model;
+
+import com.mg.merp.document.model.DocSpec;
+
+/**
+ * Базовый класс спецификаций складских документов
+ *
+ * @author Oleg V. Safonov
+ * @version $Id: BaseStockDocumentSpec.java,v 1.1 2007/09/27 15:38:46 safonov Exp $
+ */
+public class BaseStockDocumentSpec extends DocSpec {
+  // Fields
+
+  private java.math.BigDecimal Cost;
+
+  private java.math.BigDecimal Discount;
+
+  private java.math.BigDecimal PriceWithDiscount;
+
+  private java.math.BigDecimal SummaWithDiscount;
+
+  private java.math.BigDecimal DocDiscount;
+
+  private java.math.BigDecimal externalDiscountValue;
+
+  // Constructors
+
+  /**
+   * default constructor
+   */
+  public BaseStockDocumentSpec() {
+  }
+
+  public java.math.BigDecimal getCost() {
+    return this.Cost;
+  }
+
+  public void setCost(java.math.BigDecimal Cost) {
+    this.Cost = Cost;
+  }
+
+  public java.math.BigDecimal getDiscount() {
+    return this.Discount;
+  }
+
+  public void setDiscount(java.math.BigDecimal Discount) {
+    this.Discount = Discount;
+  }
+
+  public java.math.BigDecimal getPriceWithDiscount() {
+    return this.PriceWithDiscount;
+  }
+
+  public void setPriceWithDiscount(java.math.BigDecimal PriceWithDiscount) {
+    this.PriceWithDiscount = PriceWithDiscount;
+  }
+
+  public java.math.BigDecimal getSummaWithDiscount() {
+    return this.SummaWithDiscount;
+  }
+
+  public void setSummaWithDiscount(java.math.BigDecimal SummaWithDiscount) {
+    this.SummaWithDiscount = SummaWithDiscount;
+  }
+
+  public java.math.BigDecimal getDocDiscount() {
+    return this.DocDiscount;
+  }
+
+  public void setDocDiscount(java.math.BigDecimal DocDiscount) {
+    this.DocDiscount = DocDiscount;
+  }
+
+  public java.math.BigDecimal getExternalDiscountValue() {
+    return externalDiscountValue;
+  }
+
+  public void setExternalDiscountValue(java.math.BigDecimal discountValue) {
+    this.externalDiscountValue = discountValue;
+  }
+
+}
