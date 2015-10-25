bb66f4a3f848e4e6109e69c9401373c4faa04f49
Valentin A. Poroxnenko <valentin.poroxnenko@gmail.com>
2015-10-25 15:45:08 +0300

    MG-5

--- zzz-parser/src/main/java/com/tb/pups/EnumAnnMapper.java
+++ zzz-parser/src/main/java/com/tb/pups/EnumAnnMapper.java
@@ -1,16 +1,15 @@
 package com.tb.pups;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
-import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
-import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -26,9 +25,10 @@ import com.github.javaparser.ast.body.EnumDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.MemberValuePair;
 import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.NormalAnnotationExpr;
 import com.github.javaparser.ast.expr.SingleMemberAnnotationExpr;
-import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.type.ReferenceType;
 
 public class EnumAnnMapper {
@@ -54,87 +54,75 @@ public class EnumAnnMapper {
 				return CONTINUE;
 			}
 
-			private void process(Path file)	throws IOException, ParseException {
+			private void process(Path file) throws IOException, ParseException {
 
 				CompilationUnit cu = Utils.extractClass(file);
 
-				final ImportDeclaration imp = new ImportDeclaration(new NameExpr("javax.persistence.Enumerated"),false, false);
-				final ClassOrInterfaceType abe = new ClassOrInterfaceType();
+				final ImportDeclaration impEn = new ImportDeclaration(
+						new NameExpr("javax.persistence.Enumerated"), false,
+						false);
+				final ImportDeclaration impET = new ImportDeclaration(
+						new NameExpr("javax.persistence.EnumType"), false,
+						false);
 
 				List<TypeDeclaration> types = cu.getTypes();
 
 				for (TypeDeclaration t : types) {
 					if (t instanceof ClassOrInterfaceDeclaration) {
 						ClassOrInterfaceDeclaration type = (ClassOrInterfaceDeclaration) t;
-						if (Utils.isTypeExtends(type, "com.mg.merp.core.model.AbstractEntity")) {
-							boolean addImport = false;
 
-							List<BodyDeclaration> members = type.getMembers();
-							for (BodyDeclaration member : members) {
-								if (member instanceof MethodDeclaration) {
-									MethodDeclaration met = (MethodDeclaration) member;
+						List<BodyDeclaration> members = type.getMembers();
+						for (BodyDeclaration member : members) {
+							if (member instanceof MethodDeclaration) {
+								MethodDeclaration met = (MethodDeclaration) member;
 
-									if (checkMethod(met)) {
-										System.out.println(met);
+								if (checkMethod(met)) {
+									AnnotationExpr ae = Utils
+											.getMethodAnnotation(met, "Column");
+									if (ae instanceof NormalAnnotationExpr) {
+										NormalAnnotationExpr nae = (NormalAnnotationExpr) ae;
+										Iterator<MemberValuePair> it = nae
+												.getPairs().iterator();
+										boolean found = false;
+										while (!found && it.hasNext()) {
+											MemberValuePair mvp = it.next();
+											if (mvp.getName().equalsIgnoreCase(
+													"columnDefinition")) {
+												it.remove();
+												found = true;
+											}
+										}
 									}
 
+									AnnotationExpr en = Utils
+											.getMethodAnnotation(met,
+													"Enumerated");
+									if (en == null) {
+										cu.getImports().add(impEn);
+										cu.getImports().add(impET);
+										en = new SingleMemberAnnotationExpr(
+												new NameExpr("Enumerated"),
+												new NameExpr("EnumType.ORDINAL"));
+										met.getAnnotations().add(en);
+									}
+									try (PrintWriter out = new PrintWriter(file
+											.toString())) {
+										out.println(cu.toString());
+									}
 								}
-							}
 
-							if (addImport) {
-								cu.getImports().add(imp);
 							}
-							/*
-							 * try(PrintWriter out = new
-							 * PrintWriter(result.toString())){
-							 * out.println(target.toString()); }
-							 */
 						}
 					}
 				}
+
 			}
 
 			private boolean checkMethod(MethodDeclaration met) {
 				boolean result = met.getName().startsWith("get")
 						&& met.getType() instanceof ReferenceType;
-				return result && enums.contains(met.getType().toString()) && (Utils.getMethodAnnotation(met, "Column") != null);
-			}
-
-			private void addAnnotation(CompilationUnit target,
-					TypeDeclaration type, MethodDeclaration met,
-					SingleMemberAnnotationExpr ae) {
-				boolean found = false;
-
-				TypeDeclaration t = Utils.getType(target, type.getName());
-				MethodDeclaration m = null;
-
-				found = t != null;
-				if (found) {
-					found = false;
-					Iterator<BodyDeclaration> mit = t.getMembers().iterator();
-					while (!found && mit.hasNext()) {
-						BodyDeclaration b = mit.next();
-						if (b instanceof MethodDeclaration) {
-							m = (MethodDeclaration) b;
-							if (met.getParameters().size() > 0) {
-								throw new RuntimeException(
-										String.format(
-												"откуда в этом методе %s.%s параметры??",
-												type, met.getName(),
-												met.getName()));
-							}
-						}
-					}
-					if (found) {
-						m.getAnnotations().add(ae);
-					}
-				}
-
-				if (!found) {
-					throw new RuntimeException(String.format(
-							"какая-то хуйня, не нашли %s.%s", type.getName(),
-							met.getName()));
-				}
+				return result && enums.contains(met.getType().toString())
+						&& (Utils.getMethodAnnotation(met, "Column") != null);
 			}
 
 			@Override
@@ -153,11 +141,13 @@ public class EnumAnnMapper {
 			public FileVisitResult visitFile(Path file, BasicFileAttributes attr)
 					throws FileNotFoundException, IOException {
 				try {
-					TypeDeclaration td = Utils.extractClass(file).getTypes().get(0);
-					if (td instanceof EnumDeclaration){
+					TypeDeclaration td = Utils.extractClass(file).getTypes()
+							.get(0);
+					if (td instanceof EnumDeclaration) {
 						enums.add(td.getName());
-					}else{
-						throw new RuntimeException(String.format("а хто це %s ?", td.getName()));
+					} else {
+						throw new RuntimeException(String.format(
+								"а хто це %s ?", td.getName()));
 					}
 				} catch (ParseException e) {
 					throw new RuntimeException(e);
